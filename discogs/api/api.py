c4d182459e0699a1ec7ace71cff6b79ba0e70304
root <root@FRLFK6001006008.corp.capgemini.com>
2024-12-06 19:29:24 +0100

    WIP discogs

--- discogs/api/api.py
+++ discogs/api/api.py
@@ -1,21 +1,26 @@
 from flask import Flask, request, jsonify, Response
+from dotenv import load_dotenv
+import os
 import discogs_client
 
+load_dotenv()
+consumer_key = os.getenv("consumer_key")
+consumer_secret = os.getenv("consumer_secret")
+d = discogs_client.Client(
+    'my_user_agent/1.0',
+    consumer_key= consumer_key,
+    consumer_secret= consumer_secret
+)
 app = Flask(__name__)
 
 @app.route('/test', methods=['GET'])
 def test():
-    d = discogs_client.Client(
-        'my_user_agent/1.0',
-        consumer_key='xxx',
-        consumer_secret='xxx'
-    )
     release = d.release(1293022)
-    print(release)
+    print("et voilà"+release)
     artists = release.artists
     #releases = d.search('iron maiden', type='artist')[0].releases
     #print(json(releases))
-    return jsonify(artists)
+    return release
 
 
 
