817003482d02cbb154e370386f72afba9f21075b
Rémi <glatech@gmx.fr>
2025-12-01 21:23:34 +0100

    WIP

--- compose.sh
+++ compose.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-export HOSTNAME=`hostname`
+mode="$1"
+shift  # Supprime le premier argument de la liste
 
-docker-compose --env-file .env.${HOSTNAME} down -v
+if [ "$mode" = "togodo" ] || [ "$mode" = "dev" ]; then
+    autres_args=("$@")  # Stocker le reste des arguments dans un tableau
+    echo "Mode : $mode"
+    echo "Autres arguments : ${autres_args[*]}"
+else
+    echo "Erreur : le premier argument doit être 'togodo' ou 'dev'" >&2
+    exit 1
+fi
 
-docker-compose --env-file .env.${HOSTNAME} up -d --build
-
+docker-compose --project-name music-data-${mode} --env-file .env.${mode} ${autres_args[*]}
