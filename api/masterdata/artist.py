b39592329279eeed6703377d20c174e57460beeb
Rémi <rcocula@free.fr>
2026-02-23 17:08:24 +0100

    WIP

--- api/masterdata/artist.py
+++ api/masterdata/artist.py
@@ -6,15 +6,6 @@ import os
 
 STRAPI_URL = os.getenv("STRAPI_URL")
 
-def get_artist_by_allmusic_id(artist_id):
-    ids = check_artist_id(artist_id)
-    if ids:
-        found_id =ids[0]
-        url = f"{STRAPI_URL}/api/artists/{found_id}"
-        resp = requests.get(url)
-        return resp.json()
-
-
 def check_artist_id(artist_id):
     url = f"{STRAPI_URL}/api/artists?filters%5Ballmusic_id%5D={artist_id}"
     
@@ -33,3 +24,29 @@ def check_artist_id(artist_id):
     ]
 
     return allmusic_ids
+
+def get_artist_by_allmusic_id(artist_id):
+    ids = check_artist_id(artist_id)
+    if ids:
+        found_id =ids[0]
+        url = f"{STRAPI_URL}/api/artists/{found_id}"
+        resp = requests.get(url)
+        return resp.json()
+
+def generate_artist_data(any_json):
+    if any_json.get("data") is not None:
+        any_json = any_json["data"]
+    return {
+        "data": {
+            "allmusic_id": any_json.get("allmusic_id"),
+            "name": any_json.get("name")
+        }
+    }
+
+def create_artist(data):
+    url = f"{STRAPI_URL}/api/artists"
+    headers = {"Content-Type": "application/json"}
+    resp = requests.post(url, headers=headers, data=json.dumps(generate_artist_data(data)))
+    if resp.status_code != 200:
+        raise RuntimeError(f"Erreur HTTP {resp.status_code} pour URL {url}")
+    return resp.json()
--- api/masterdata_routes.py
+++ api/masterdata_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from idagiograbber.album import *
 from dotenv import load_dotenv
-from masterdata.artist import check_artist_id, get_artist_by_allmusic_id
+from masterdata.artist import check_artist_id, get_artist_by_allmusic_id, create_artist as create_artist_func
 
 load_dotenv()
 
@@ -11,13 +11,22 @@ VERIFY_SSL = os.getenv("VERIFY_SSL")
 DATA_DIR = os.getenv("DATA_DIR")
 
 @masterdata_bp.route("/artist", methods=["GET"])
-def artist():
+def get_artist():
     artist_id = request.args.get("artist_id")
     if not artist_id:
         return jsonify({"error": "Query parameter 'artist_id' is required"}), 400
 
     try:
         result = get_artist_by_allmusic_id(artist_id)
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@masterdata_bp.route("/artist", methods=["POST"])    
+def create_artist():
+    data = request.get_json()
+    try:
+        result = create_artist_func(data)
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
