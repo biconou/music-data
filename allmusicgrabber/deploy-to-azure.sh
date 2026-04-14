4610cfd1ce72e295befe424035339b2565e9f22d
Rémi <rcocula@free.fr>
2026-04-14 13:06:30 +0200

    Menage

--- allmusicgrabber/deploy-to-azure.sh
+++
@@ -1,4 +0,0 @@
-
-docker build -t allmusicgrabber:latest .
-
-az webapp create --name allmusicgrabber-api --resource-group music-data-dev --plan music-data --deployment-container-image-name allmusicgrabber:latest
